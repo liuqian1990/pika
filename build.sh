1d26be0d4aa06577bc845e3041c690ffb0a3ad58
richie <rliuqian@gmail.com>
2023-04-11 17:48:40 +0800

    Update build.sh
    MAC上处理无/proc/cpuinfo
    -- Build files have been written to: /Users/admin/pika/output
    cat: /proc/cpuinfo: No such file or directory

--- build.sh
+++ build.sh
@@ -1,71 +1,45 @@
 #!/bin/bash
 
-#color code
-C_RED="\033[31m"
-C_GREEN="\033[32m"
-
-C_END="\033[0m"
-
-CMAKE_MIN_VERSION="3.18"
-TAR_MIN_VERSION="1.34"
+MIN_VERSION="3.10"
 
 BUILD_DIR=output
 
 source ./utils/Get_OS_Version.sh
 
 function version_compare() {
-    if [[ "$1" == "$2" ]]; then
+    if [[ "${MIN_VERSION}" == "$1" ]]; then
         return 0
     fi
 
-    if [[ "$(printf '%s\n' "$1" "$2" | sort -rV | head -n1)" == "$1" ]]; then
+    if [[ "$(printf '%s\n' "${MIN_VERSION}" "$1" | sort -rV | head -n1)" == "${MIN_VERSION}" ]]; then
         #local version less min version
-        echo -e "local ${C_GREEN} $3 ${C_END} version ${C_GREEN} $2 ${C_END} less min version ${C_GREEN} $1 ${C_END}"
+        echo -e "local cmake version \033[32m $1 \033[0m less min version \033[32m ${MIN_VERSION} \033[0m"
         exit 1
     fi
 }
 
-function check_program() {
-    if ! type $1 >/dev/null 2>&1; then
-        # not find
-        echo -e "not find ${C_GREEN} $1 ${C_END} on localhost"
-        return 1
-    fi
-    return 0
-}
-
-function install_package() {
+if ! type autoconf >/dev/null 2>&1; then
+    # not find autoconf,do install
+    echo -e "not find \033[32m autoconf \033[0m on localhost, now do install"
     if [ $PM == "unknow" ]; then
-        echo -e "${C_RED} unknow package manager, please install $1 ${C_END}"
-        exit 1
+        echo -e "\033[31m unknow package manager, please install autoconf \033[0m"
     fi
     if [ ${PM} == "apt" ]; then
-      sudo ${PM} -y install $1
+      sudo ${PM} -y install autoconf
     elif [ ${PM} == "brew" ]; then
-      ${PM} install -d $1
+      ${PM} install -d autoconf
     else
-      sudo ${PM} install -y $1
+      sudo ${PM} install -y autoconf
     fi
     if [ $? -ne 0 ]; then
-        echo -e "${C_RED} install $1  fail, install autoconf before compiling ${C_END}"
+        echo -e "\033[31m install autoconf  fail, install autoconf before compiling \033[0m"
         exit 1;
     fi
-}
-
-if ! check_program autoconf; then
-    # not find autoconf,do install
-    echo -e "not find ${C_GREEN} autoconf ${C_END} on localhost, now do install"
-    install_package autoconf
 fi
 
-if ! check_program tar; then
-    echo -e "not find ${C_GREEN} tar ${C_END} on localhost, please install and min version ${C_GREEN} ${TAR_MIN_VERSION} ${C_END}"
-    exit 1;
-fi
-
-if ! check_program cmake; then
-    if ! check_program cmake3; then
-        echo -e "not find ${C_GREEN} cmake ${C_END}, please install cmake and min version ${C_GREEN} ${CMAKE_MIN_VERSION} ${C_END}"
+if ! type cmake >/dev/null 2>&1; then
+    if ! type cmake >/dev/null 2>&1; then
+        echo "not find cmake, please install cmake and min version \033[32m ${MIN_VERSION} \033[0m"
         exit 1
     else
         CMAKE=cmake3
@@ -74,15 +48,9 @@ else
     CMAKE=cmake
 fi
 
-# get local cmake version
-LOCAL_CMAKE_VERSION=`${CMAKE} --version |grep version |grep -o '[0-9.]\+'`
-#compare cmake version
-version_compare ${CMAKE_MIN_VERSION} ${LOCAL_CMAKE_VERSION} 'cmake'
+LOCAL_VERSION=`${CMAKE} --version |grep version |grep -o '[0-9.]\+'`
 
-# get local tar version
-LOCAL_TAR_VERSION=`tar --version |head -n 1 |grep -o '[0-9.]\+'`
-#compare tar version
-version_compare ${TAR_MIN_VERSION} ${LOCAL_TAR_VERSION} 'tar'
+version_compare ${LOCAL_VERSION}
 
 if [ ! -d ${BUILD_DIR} ]; then
     mkdir ${BUILD_DIR}
@@ -93,11 +61,16 @@ cd ${BUILD_DIR}
 ${CMAKE} .. .
 
 if [ $? -ne 0 ]; then
-    echo -e "${C_RED} cmake execution error ${C_END}"
+    echo -e "\033[31m cmake execution error \033[0m"
     exit 1
 fi
 
-CPU_CORE=`cat /proc/cpuinfo| grep "processor"| wc -l`
+if [ ! -f "/proc/cpuinfo" ];then
+  CPU_CORE=$(sysctl -n hw.ncpu)
+else
+  CPU_CORE=$(cat /proc/cpuinfo| grep "processor"| wc -l)
+fi
+
 if [ ${CPU_CORE} -eq 0 ]; then
   CPU_CORE=1
 fi
@@ -107,5 +80,5 @@ echo "cpu core ${CPU_CORE}"
 make -j ${CPU_CORE}
 
 if [ $? -eq 0 ]; then
-    echo -e "pika compile complete, output file ${C_GREEN} ${BUILD_DIR}/pika ${C_END}"
+    echo -e "pika compile complete, output file \033[32m ${BUILD_DIR}/pika \033[0m"
 fi
